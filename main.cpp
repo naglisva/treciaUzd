6b6809ed34ef624e62e87f45e2784c8d9ed5a2f0
nagli <naglis.vaiciulionis@gmail.com>
2023-11-03 20:09:29 +0200

    paruosta atsiskaitymui

--- main.cpp
+++ main.cpp
@@ -10,46 +10,46 @@ int main() {
 	skirstymas2("pirmas.txt","geri1.txt", "blogi1.txt");
 	skirstymas2("antras.txt", "geri2.txt", "blogi2.txt");
 	skirstymas2("trecias.txt", "geri3.txt", "blogi3.txt");
-	skirstymas2("ketvirtas.txt", "geri4.txt", "blogi4.txt");
-	skirstymas2("penktas.txt", "geri5.txt", "blogi5.txt");
+	//skirstymas2("ketvirtas.txt", "geri4.txt", "blogi4.txt");
+	//skirstymas2("penktas.txt", "geri5.txt", "blogi5.txt");
 	cout << endl;
 
 	cout << "Naudojant vektoriu " << endl;
 	skirstymas("pirmas.txt","geri1v.txt", "blogi1v.txt");
 	skirstymas("antras.txt", "geri2v.txt", "blogi2v.txt");
 	skirstymas("trecias.txt", "geri3v.txt", "blogi3v.txt");
-	skirstymas("ketvirtas.txt", "geri4v.txt", "blogi4v.txt");
-	skirstymas("penktas.txt", "geri5v.txt", "blogi5v.txt");
+	//skirstymas("ketvirtas.txt", "geri4v.txt", "blogi4v.txt");
+	//skirstymas("penktas.txt", "geri5v.txt", "blogi5v.txt");
 	cout << endl;
 	
 	cout << "Naudojant vektoriu, 2 strategija " << endl;
 	nskirstymas("pirmas2.txt", "nblogi1v.txt");
 	nskirstymas("antras2.txt", "nblogi2v.txt");
 	nskirstymas("trecias2.txt", "nblogi3v.txt");
-	nskirstymas("ketvirtas2.txt", "nblogi4v.txt");
-	nskirstymas("penktas2.txt", "nblogi5v.txt");
+	//nskirstymas("ketvirtas2.txt", "nblogi4v.txt");
+	//nskirstymas("penktas2.txt", "nblogi5v.txt");
 	cout << endl;
 
 	cout << "Naudojant sarasa, 2 strategija " << endl;
 	nskirstymas2("pirmas3.txt", "nblogi1l.txt");
 	nskirstymas2("antras3.txt", "nblogi2l.txt");
 	nskirstymas2("trecias3.txt", "nblogi3l.txt");
-	nskirstymas2("ketvirtas3.txt", "nblogi4l.txt");
-	nskirstymas2("penktas3.txt", "nblogi5l.txt");
+	//nskirstymas2("ketvirtas3.txt", "nblogi4l.txt");
+	//nskirstymas2("penktas3.txt", "nblogi5l.txt");
 	cout << endl;
 	
 	cout << "Naudojant vektoriu, 3 strategija " << endl;
 	nskirstymasv("pirmas4.txt", "nblogi1v.txt");
 	nskirstymasv("antras4.txt", "nblogi2v.txt");
 	nskirstymasv("trecias4.txt", "nblogi3v.txt");
-	nskirstymasv("ketvirtas4.txt", "nblogi4v.txt");
-	nskirstymasv("penktas4.txt", "nblogi5v.txt");
+	//nskirstymasv("ketvirtas4.txt", "nblogi4v.txt");
+	//nskirstymasv("penktas4.txt", "nblogi5v.txt");
 	cout << endl;
 	
 	cout << "Naudojant sarasa, 3 strategija " << endl;
 	nskirstymasl("pirmas.txt","geri1l.txt", "blogi1l.txt");
 	nskirstymasl("antras.txt", "geri2l.txt", "blogi2l.txt");
 	nskirstymasl("trecias.txt", "geri3l.txt", "blogi3l.txt");
-	nskirstymasl("ketvirtas.txt", "geri4l.txt", "blogi4l.txt");
-	nskirstymasl("penktas.txt", "geri5l.txt", "blogi5l.txt");
+	//nskirstymasl("ketvirtas.txt", "geri4l.txt", "blogi4l.txt");
+	//nskirstymasl("penktas.txt", "geri5l.txt", "blogi5l.txt");
 }
